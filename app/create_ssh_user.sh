684911c4d6a917c66062a2e22016b10f42b3815b
seshagiri96 <seshu.nine6@gmail.com>
2016-08-03 00:33:25 +0530

    Few changes made to server.js , included script for creating user

---
+++ app/create_ssh_user.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+#
+# #############################################################################
+# Create new SSH user (Ubuntu)
+# I use this script whenever I need to add a new SSH user to an Ubuntu machine.
+# Usage:
+# 1)  Download the file 
+# 2)  Make it executable with -  chmod a+x createNewSSHUser.sh  
+# 3)  Execute it with : sudo ./createNewSSHUser.sh
+# 4)  Immediately set a new password by logging in once with -
+#          su newUsrName
+# #############################################################################
+#
+   #
+   touch /root/trash 2> /dev/null
+   if [  $? -ne  0  ]
+   then 
+      echo "error: Must execute as root user !!! "
+      exit 0;
+   fi
+   export A_NEW_USER=$1
+   export NEW_USER_PWD=$2
+   #
+   echo New User is $A_NEW_USER identified by $NEW_USER_PWD
+   #
+   echo "Get ${A_NEW_USER} home directory .. . . . . . . . "
+   export A_NEW_USER_HOME=$( grep "${A_NEW_USER}" /etc/passwd | awk -F: '{print $6}' )
+   echo "New user's home directory is ${A_NEW_USER_HOME}"
+   #
+   if [ "XX${A_NEW_USER_HOME}" == "XX" ]; then
+
+   export PASS_HASH=$(perl -e 'print crypt($ARGV[0], "password")' "$NEW_USER_PWD")
+   #echo ${PASS_HASH}
+   
+   # addgroup imad-dev
+   useradd -Ds /bin/bash
+   useradd -m -G imad-dev -p ${PASS_HASH} ${A_NEW_USER}
+   passwd -e ${A_NEW_USER}
+   #
+   A_NEW_USER_HOME=/home/${A_NEW_USER}
+   else
+   echo "The ${A_NEW_USER} user account is already configured in ${A_NEW_USER_HOME} . . . "
+   exit 0
+   fi
+   #
+   echo "................................................................"
+   echo "Prepare for SSH tasks"
+   echo "................................................................"
+
+   export A_NEW_USER_SSH_DIR=${A_NEW_USER_HOME}/.ssh
+   mkdir -p ${A_NEW_USER_SSH_DIR}
+   chmod 700 ${A_NEW_USER_SSH_DIR}
+   #
+   pushd ${A_NEW_USER_SSH_DIR}
+   #
+     #
+     echo "................................................................"
+     echo "Generate SSH key pair for ${A_NEW_USER}"
+     echo "................................................................"
+     rm -f id_rsa*
+     ssh-keygen -f id_rsa -t rsa -N '' -C "${A_NEW_USER}@${A_NEW_USER}.me"
+     #
+   #
+   popd
+   #
+   echo "................................................................"
+   echo "Assign correct ownership ......................................"
+   echo "................................................................"
+   #chown -R ${A_NEW_USER}:imad-dev /home/${A_NEW_USER}
+   chmod 700 /home/${A_NEW_USER}
+   #
+   echo "................................................................"
+   echo "Here is ${A_NEW_USER}'s public key"
+   echo "................................................................"
+   echo " "
+   cat ${A_NEW_USER_SSH_DIR}/id_rsa.pub
+   echo " "
+   echo " "
+   echo " "
+   echo Done creating new user ${A_NEW_USER}
+   echo   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+exit 0
--- app/src/server.js
+++ app/src/server.js
@@ -3,9 +3,13 @@ import path from 'path';
 // import PrettyError from 'pretty-error';
 import http from 'http';
 import morgan from 'morgan';
-import fetch from 'node-fetch';
+
+import bodyParser from 'body-parser';
+// import fetch from 'node-fetch';
 
 import config from './config';
+
+const exec = require('child_process').exec;
 
 // const pretty = new PrettyError();
 const app = new Express();
@@ -17,23 +21,77 @@ if (global.__DEVELOPMENT__)
 else
   app.use(morgan('[:date[clf]]: :method :url :status :res[content-length] - :response-time ms'));
 
+app.use(bodyParser.json());
 app.use('/static', Express.static(path.join(__dirname, '..', 'static')));
 
-app.get('/create-user', (req, res) => {
-  fetch('https://httpbin.org/ip').then(
-    (response) => {
-      response.text().then((data) => {
-        console.log(data);
-        const ip = JSON.parse(data);
-        res.send(ip.origin);
-      });
-    },
-    (error) => {
-      console.error(error);
-      res.status(500).send('something went wrong');
-    });
+// app.get('/create-user', (req, res) => {
+//   fetch('https://httpbin.org/ip').then(
+//     (response) => {
+//       response.text().then((data) => {
+//         console.log(data);
+//         const ip = JSON.parse(data);
+//         res.send(ip.origin);
+//       });
+//     },
+//     (error) => {
+//       console.error(error);
+//       res.status(500).send('something went wrong');
+//     });
+// });
+
+app.post('/create-user', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  exec(`./create_ssh_user1.sh ${username} ${password}`, (error, stdout, stderr) => {
+    if (!error) {
+      res.send('User created!!');
+      console.log('User created', username, password);
+      if (stdout) {
+        console.log(`stdout : ${stdout}`);
+      }
+      if (stderr) {
+        console.log(`stderr : ${stderr}`);
+      }
+    } else {
+      console.log('User not created!!');
+      console.log('Error: ', error);
+      res.send('Internal error occured executing the script useradd!!');
+    }
+  });
 });
 
+app.post('/delete-user', (req, res) => {
+  const username = req.body.username;
+  exec(`./delete_ssh_user.sh ${username}`, (error, stdout, stderr) => {
+    if (!error) {
+      console.log('User deleted', username);
+      res.send('User deleted Successfully');
+    } else {
+      console.log('User not deleted');
+      console.log('Error: ', error);
+      res.send('Internal error during command executing  userdel!!')
+    }
+  });
+});
+
+app.post('/update-ssh-password', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  exec(`./update_ssh_password.sh ${username} ${password}`, (error, stdout, stderr) => {
+    if (!error) {
+      console.log('User password updated Successfully', username);
+      res.send('User password updated Successfully');
+    } else {
+      console.log('Failed to update password');
+      console.log('Error: ', error);
+      res.send('Internal error during command executing passwd')
+    }
+  });
+});
+
+app.post('/update-ssh-password', (req, res) => {
+  res.send('Password Updated Successfully!!');
+});
 
 // Listen at the server
 if (config.port) {
